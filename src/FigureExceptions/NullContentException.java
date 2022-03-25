b54db5d1c02dd2f755575ff9d1a2f53a12541d87
Salvador García <Salvador Garca@DESKTOP-O96DQO8>
2022-03-25 17:59:11 -0600

    Creando las clases

---
+++ src/FigureExceptions/NullContentException.java
@@ -0,0 +1,12 @@
+package FigureExceptions;
+
+/*
+@author García Romero Salvador
+@author Martínez González Ariel
+@author Morales Tapia Misael
+@author Vega Macías Fernando Manuel
+*/
+
+public class NullContentException {
+    
+}
